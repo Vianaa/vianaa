018c78a0365c982f0af6160ab1631d8ca07ff460
Vianaa <43130359+Vianaa@users.noreply.github.com>
2018-09-10 14:16:35 +0700

    Add files via upload

---
+++ viana/src/viana/Viana.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viana;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Viana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //LOOPING
+        int jumlah = 5;
+        for (int i=1; i<=jumlah;i++){
+            for (int a=0; a<i; a++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+    
+}
